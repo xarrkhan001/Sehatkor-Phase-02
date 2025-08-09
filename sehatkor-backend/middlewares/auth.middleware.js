977fb66631735b9cd54d45c24cd48fb7be078d07
Abuzar <abuzarktk123@gmail.com>
2025-08-08 21:54:24 -0700

    disease and syptom bug fixed

--- sehatkor-backend/middlewares/auth.middleware.js
+++ sehatkor-backend/middlewares/auth.middleware.js
@@ -1,10 +1,12 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1]; // Expecting "Bearer TOKEN"
+  const token = req.headers.authorization?.split(" ")[1]; // Expecting "Bearer TOKEN"
 
   if (!token)
-    return res.status(401).json({ message: 'Access denied. No token provided.' });
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -12,7 +14,7 @@ const authMiddleware = (req, res, next) => {
     req.userRole = decoded.role;
     next();
   } catch (err) {
-    res.status(400).json({ message: 'Invalid token' });
+    res.status(400).json({ message: "Invalid token" });
   }
 };
 
